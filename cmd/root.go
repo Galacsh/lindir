43824467ed0934a677527b4e0bb5bb26e943ea80
Galacsh <galacsh.dev@gmail.com>
2023-12-20 00:40:01 +0900

    feat: tool description

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"lindir/app"
+	"lindir/common/constants"
 	"lindir/common/types"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -20,14 +22,9 @@ type App interface {
 var lindir App
 
 var rootCmd = &cobra.Command{
-	Use:   "lindir",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   constants.CMD,
+	Short: rootCmdShort(),
+	Long:  rootCmdLong(),
 }
 
 func Execute() {
@@ -38,4 +35,32 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func rootCmdShort() string {
+	return "Tool for managing hard links across multiple directories"
+}
+
+func rootCmdLong() string {
+	description := `
+{{APP}} is a tool for managing hard links across multiple directories.
+
+It is designed to address the limitations of traditional hard links and
+symbolic links. While hard links allow multiple access points to a single file,
+they cannot be used for directories. Symbolic links, on the other hand, can
+link directories but may not always be compatible with certain programs.
+
+To overcome these limitations, {{APP}} allows you to mimic directory linking
+through a Git-inspired approach. {{APP}} tracks and manages hard links by
+creating a '{{APP_DIR}}' directory within your chosen directory. This approach
+is particularly beneficial in scenarios where you require a single source of
+truth for your files, such as managing dot files in a single Git repository.
+
+Run '{{CMD}} help <command>' to learn more about a specific command.
+`
+
+	description = strings.ReplaceAll(description, "{{APP}}", constants.APP)
+	description = strings.ReplaceAll(description, "{{CMD}}", constants.CMD)
+	description = strings.ReplaceAll(description, "{{APP_DIR}}", constants.APP_DIR)
+	return strings.TrimSpace(description)
 }
