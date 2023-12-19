063fc7ef73ee9b11d4d157e57b9ac8673ea28a33
Galacsh <galacsh.dev@gmail.com>
2023-12-20 00:25:04 +0900

    feat: app interface

---
+++ app/app.go
@@ -0,0 +1,39 @@
+package app
+
+import "lindir/common/types"
+
+type lindir struct{}
+
+func New() lindir {
+	return lindir{}
+}
+
+type notImplementedError struct{}
+
+func (e notImplementedError) Error() string {
+	return "not implemented"
+}
+
+func (l lindir) Init(dir types.Path) error {
+	return &notImplementedError{}
+}
+
+func (l lindir) Status(dir types.Path) (types.PathSet, types.PathSet, error) {
+	return nil, nil, &notImplementedError{}
+}
+
+func (l lindir) Link(fromDir, toDir types.Path) error {
+	return &notImplementedError{}
+}
+
+func (l lindir) Unlink(dir types.Path) error {
+	return &notImplementedError{}
+}
+
+func (l lindir) Push(dir types.Path) error {
+	return &notImplementedError{}
+}
+
+func (l lindir) Sync(dir types.Path) error {
+	return &notImplementedError{}
+}
