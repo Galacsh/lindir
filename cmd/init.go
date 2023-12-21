57e50b072b9d4d05ceddf56bc18630ac5709a79c
Galacsh <galacsh.dev@gmail.com>
2023-12-21 22:37:27 +0900

    feat: init command description

--- cmd/init.go
+++ cmd/init.go
@@ -4,6 +4,7 @@ import (
 	"lindir/common/constants"
 	"lindir/common/types"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -33,9 +34,28 @@ func init() {
 }
 
 func initCmdShort() string {
-	return ""
+	return "Initialize a directory for use with " + constants.APP
 }
 
 func initCmdLong() string {
-	return ""
+	description := `
+{{APP}} requires a '{{APP_DIR}}' directory to track and manage hard links.
+
+This command creates '{{APP_DIR}}' directory inside current working directory.
+Inside the '{{APP_DIR}}' directory, you will find two files:
+
+* 'connector'
+  - This file shows directories that are linked to the current directory.
+  - Will be automatically updated when you run the 'link' or 'unlink' commands.
+  - Right after initialization, this file will contain current directory.
+  - Do not modify this file manually.
+* 'tracker'
+  - This file shows what files are currently hard linked across directories.
+  - Will be automatically updated when you run the 'push' or 'sync' commands.
+  - Right after initialization, this file will be empty.
+  - Do not modify this file manually.
+`
+	description = strings.ReplaceAll(description, "{{APP}}", constants.APP)
+	description = strings.ReplaceAll(description, "{{APP_DIR}}", constants.APP_DIR)
+	return strings.TrimSpace(description)
 }
