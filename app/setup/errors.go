79646fd0923ba42e66ccd6658d54319f7f990805
Galacsh <galacsh.dev@gmail.com>
2023-12-23 19:17:17 +0900

    feat: implement setup functions

--- app/setup/errors.go
+++ app/setup/errors.go
@@ -1,4 +1,4 @@
-package initialize
+package setup
 
 import (
 	"fmt"
--- app/setup/setup.go
+++ app/setup/setup.go
@@ -27,14 +27,59 @@ func CreateTrackerFile(dir types.Path) error {
 	return track.TrackerFileOf(dir).Write(types.PathSet{})
 }
 
+func noTrackerFile(dir types.Path) (bool, error) {
+	return track.TrackerFileOf(dir).NotExists()
+}
+
+func noConnectorFile(dir types.Path) (bool, error) {
+	return connect.ConnectorFileOf(dir).NotExists()
+}
+
 func IsNotInitialized(dir types.Path) (bool, error) {
-	return false, nil
+	noTracker, err := noTrackerFile(dir)
+	if err != nil {
+		return false, err
+	}
+
+	noConnector, err := noConnectorFile(dir)
+	if err != nil {
+		return false, err
+	}
+
+	return noTracker || noConnector, nil
+}
+
+func isInitialized(dir types.Path) (bool, error) {
+	notInitialized, err := IsNotInitialized(dir)
+	if err != nil {
+		return false, err
+	}
+
+	return !notInitialized, nil
 }
 
 func ErrIfNotInitialized(dir types.Path) error {
-	return &notInitializedError{dir}
+	notInitialized, err := IsNotInitialized(dir)
+	if err != nil {
+		return err
+	}
+
+	if notInitialized {
+		return &notInitializedError{dir}
+	} else {
+		return nil
+	}
 }
 
 func ErrIfAlreadyInitialized(dir types.Path) error {
-	return &alreadyInitializedError{dir}
+	initialized, err := isInitialized(dir)
+	if err != nil {
+		return err
+	}
+
+	if !initialized {
+		return &alreadyInitializedError{dir}
+	} else {
+		return nil
+	}
 }
